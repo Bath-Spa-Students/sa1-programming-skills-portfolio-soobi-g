f4e16097ee25cea8b52079663891443d03a43d01
soobi-g <boucifsoheibg@gmail.com>
2024-11-18 11:56:30 +0400

    biography.py

--- Excercice 03/Biography.py
+++ Excercice 03/Biography.py
@@ -1,4 +1,7 @@
 #storing informations
-info ={"name":" BOUCIF","age": 19,"hometown":"rasalkhaimah"}
-print(info)
+info ={"name":" BOUCIF",
+       "age": 19,
+       "hometown":"rasalkhaimah"}
+#output on seperate lines
+print(f"Name: {info.get('name')} \nAge: {info.get('age')} \nHometown: {info.get('hometown')}")
 
