dcc2b7b331a6fe9482a4dad520dd703240b6e2ef
soobi-g <boucifsoheibg@gmail.com>
2024-11-20 23:18:49 +0400

    Update biography advanced requirements.py

--- Excercice 03/biography advanced requirements.py
+++ Excercice 03/biography advanced requirements.py
@@ -1,19 +1,17 @@
-# 
+# getting inpit
 first_name = input("what's your first name? ")
 second_name = input("what's your second name? ")
 
 hometown = input("from where you are? ")
 
-# 
+# validating age
 age = input("how old are you? ")
 while not age.isdigit():
     age = input("enter your number age: ")
 
-
+#recording infos
 info ={"name": first_name + " " + second_name,
        "age": age,
        "hometown": hometown}
-
+#getting the result
 print(f"Name: {info.get('name')} \nAge: {info.get('age')} \nHometown: {info.get('hometown')}")
-
-
