b1d7a0c1e77059db584a2ee1f83f99aaf3234002
soobi-g <boucifsoheibg@gmail.com>
2024-11-21 10:27:13 +0400

    6.py

---
+++ Bonus/6.py
@@ -0,0 +1,14 @@
+#dictionary storing person informauions
+person_info = {
+    'first_name': 'sifou',
+    'last_name': 'dekmmous',
+    'age': 18,
+    'city': 'tlemcen'
+}
+
+#printing info
+print(person_info['first_name'])
+print(person_info['last_name'])
+print(person_info['age'])
+print(person_info['city'])
+
