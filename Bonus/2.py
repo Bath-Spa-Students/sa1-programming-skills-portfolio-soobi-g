faf6a02fe3474fb21f61a6c45089d50dba36a12a
soobi-g <boucifsoheibg@gmail.com>
2024-11-21 10:19:41 +0400

    2.py

---
+++ Bonus/2.py
@@ -0,0 +1,7 @@
+#assign a message to a variable, and orint it
+message = "hello bath spa university"
+print(message)
+
+# change variable s value to a new message and printing it.
+message = "have a good year"
+print(message)
